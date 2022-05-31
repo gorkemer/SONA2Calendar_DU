aef5faa46b71ad29d81b07c5344f35d3baa56296
gorkemer <gorkemerr@gmail.com>
2022-05-31 17:40:52 -0600

    included du website instead of DU, includeed study names

--- constants.py
+++ constants.py
@@ -1,19 +1,18 @@
 # URL of your SONA system
-sona_domain = 'https://ucla.sona-systems.com/'
+sona_domain = 'https://du.sona-systems.com/'
 
 # A file that includes your SONA username (on the 1st line) and password (on the 2nd line)
 sona_credentials_file = 'sona_credentials.txt'
 
 # Google calendar id (find it in your Calendar Settings -> Calendar ID)
 # Make sure the keys in this dictionary match the study names in the SONA system
-google_calendar_ids = {'Faces and Reasoning Study': 'gipgc18l73j59bolk7u32doeug@group.calendar.google.com',
-                       'Relational Knowledge': 'ph2o3oda2nj424d1tg8r72djho@group.calendar.google.com'}
+google_calendar_ids = {'Motion-induced Grouping': 'epk6uuv1fo3a3s6afi2cvo01o4@group.calendar.google.com',
+						}
 
 # This dictionary defines the study names to be displayed in the calendar, if different from the study names in SONA.
 # Example: {"A long and unclear study name being used in the SONA system": "Short Study Name"}
 # Change this to an empty dictionary if you want the study names to be same as in the SONA system
-calendar_study_names = {'Faces and Reasoning Study': 'Faces Study',
-                        'Relational Knowledge': 'Net Learn'}
+calendar_study_names = {}
 
 # Color scheme for calendar events: if all words (separated by spaces) in a key string of this dictionary can be
 # found in the information of a study timeslot, the corresponding color (value in this dictionary) will be applied
